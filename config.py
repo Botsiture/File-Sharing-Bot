e93a7b9270160fbea8b8927767a66dbced2a329a
Botsiture <bismillaha443@gmail.com>
2025-01-19 07:13:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "AAFTE1M7qVQpNATJh4wsuWxjCifVcvzdUJM")
+API_ID = int(os.environ.get("API_ID", "10658015"))
+API_HASH = os.environ.get("API_HASH", "a0087bca748f86698c53d291c9e5b3af")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "mongodb+srv://saverestrictionsbot:ntxdcDZXhROjC4C7@cluster0.dh8r0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_NAME = os.environ.get("DB_NAME", "sajidbotz")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002256631540"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 
@@ -28,8 +28,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[6848088376]
-    for x in (os.environ.get("ADMINS", "6848088376").split()):
+    ADMINS=[6170962395]
+    for x in (os.environ.get("ADMINS", "6170962395").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -67,7 +67,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need 
 
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6848088376)
+ADMINS.append(6170962395)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
